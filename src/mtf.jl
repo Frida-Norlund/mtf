a77d252f7b344831ac97e917168c2b786f1e4378
Frida-Norlund <82037342+Frida-Norlund@users.noreply.github.com>
2022-10-03 16:15:33 +0200

    adding code

--- src/mtf.jl
+++ src/mtf.jl
@@ -1,5 +1,39 @@
 module mtf
 
-# Write your package code here.
+export TrackingFloat
+
+struct TrackingFloat <: AbstractFloat
+    value::Float64
+    tracked_value::Float64 
+end
+
+TrackingFloat(x) = TrackingFloat(x,zero(x))
+
+value(TrackingFloat) = TrackingFloat.value
+
+getmax(TrackingFloat) = TrackingFloat.tracked_value
+
+
+import Base: +, *, -, /
+
+
++(a::TrackingFloat, b::TrackingFloat) = TrackingFloat(a.value + b.value, maximum(abs.([a.tracked_value a.value b.tracked_value b.value])))
+
+-(a::TrackingFloat, b::TrackingFloat) = TrackingFloat(a.value - b.value, maximum(abs.([a.tracked_value a.value b.tracked_value b.value])))
+
+*(a::TrackingFloat, b::TrackingFloat) = TrackingFloat(a.value * b.value, maximum(abs.([a.tracked_value a.value b.tracked_value b.value])))
+
+/(a::TrackingFloat, b::TrackingFloat) = TrackingFloat(a.value * 1/b.value, maximum(abs.([a.tracked_value a.value b.tracked_value 1/b.value])))
+
+
+Base.promote_rule(::Type{TrackingFloat}, ::Type{<:Real})  = TrackingFloat
+TrackingFloat(a::TrackingFloat) = a
+
+import Base: sqrt, <, -
+
+sqrt(a::TrackingFloat) = TrackingFloat(sqrt(a.value),a.tracked_value)
+<(a::TrackingFloat, b::TrackingFloat) = <(a.value,  b.value) 
+-(a::TrackingFloat) = TrackingFloat(-a.value , a.tracked_value)
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,55 @@ using mtf
 using Test
 
 @testset "mtf.jl" begin
-    # Write your tests here.
+    
+
+    v = TrackingFloat(1.0) + TrackingFloat(3.0) # We expect TrackingFloat(4, 3)
+    @test v     == TrackingFloat(4,3)           # which we test using the macro @test
+    @test v*v   == TrackingFloat(16, 4)
+    @test v - v == TrackingFloat(0, 4)
+    @test v/TrackingFloat(0.1, 0) == TrackingFloat(40, 10)
+
+    # Try working with matrices
+
+    A = randn(10,10)
+    b = randn(10)
+
+    # Convert using broadcast
+    At = TrackingFloat.(A)
+    bt = TrackingFloat.(b)
+
+    # Try some operations
+    v = A*b
+    vt = At*bt
+    # Did we calculate correctly? Using value to convert back to float
+    @test maximum(abs, v - value.(vt)) < sqrt(eps())
+
+    # Get the max fields using our function getmax
+    getmax.(vt)
+
+
+
+    # Is promotion working?
+    @test TrackingFloat(1.0, 0) + 2.0 == TrackingFloat(3, 2)
+    @test TrackingFloat(1.0, 0) + 2 == TrackingFloat(3, 2)
+
+    # Create Positive definite matrix
+    AA = A*A'
+    # Convert to TrackingFloat matrix
+    AAt = TrackingFloat.(AA)
+
+    sol1 = AAt\bt # Uses qr
+    # Did we get the correct answer?
+    @test maximum(abs, value.(sol1) - AA\b) < sqrt(eps())
+
+    # Try cholesky factorization
+    F = cholesky(AAt)
+
+    sol2 = F\bt
+    @test maximum(abs, value.(sol2) - AA\b) < sqrt(eps())
+
+    # Which method was able to work with smallest elements?
+    maximum(getmax.(sol1))
+    maximum(getmax.(sol2))
+
 end
